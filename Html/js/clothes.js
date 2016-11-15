1edc7c245eb91915813b7420ff7ff3b4fc7e4bba
尹建华 <yinjianhua@meizu.com>
2016-11-16 00:40:31 +0800

    1.优化登录页面的显示;2.增加衣柜图片的边框显示

--- Html/js/clothes.js
+++ Html/js/clothes.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(window).load(function () {
         imgLocation();
     });
@@ -7,29 +7,32 @@ $(document).ready(function(){
 });
 
 
-function imgLocation(){
+function imgLocation() {
     var box = $(".img_box");
     var boxWidth = box.eq(0).width();
-    var num = Math.floor($(".wardrobe").width()/boxWidth);
-    var boxArr=[];
-    box.each(function(index,value){
-//        console.log(index+"--"+value);
+    var num = Math.floor($(".wardrobe").width() / boxWidth);
+    var boxArr = [];
+    var maxHeight = 0;
+    box.each(function (index, value) {
         var boxHeight = box.eq(index).height();
-        if(index<num){
-            boxArr[index]= boxHeight;
-            console.log(boxHeight);
-        }else{
-            var minboxHeight = Math.min.apply(null,boxArr);
-            console.log(minboxHeight);
-            var minboxIndex = $.inArray(minboxHeight,boxArr);
-            //console.log(minboxIndex);
-//            console.log(value);
+        if (index < num) {
+            boxArr[index] = boxHeight;
+            if (boxHeight > maxHeight) maxHeight = boxHeight;
+        } else {
+            var minboxHeight = Math.min.apply(null, boxArr);
+            var minboxIndex = $.inArray(minboxHeight, boxArr);
             $(value).css({
-                "position":"absolute",
-                "top":minboxHeight,
-                "left":box.eq(minboxIndex).position().left
+                "position": "absolute",
+                "top": minboxHeight,
+                "left": box.eq(minboxIndex).position().left
             });
-            boxArr[minboxIndex]+=box.eq(index).height();
+            boxArr[minboxIndex] += box.eq(index).height();
+            if (boxArr[minboxIndex] > maxHeight) maxHeight = boxArr[minboxIndex];
         }
+    });
+    console.log("Max Height: " + maxHeight);
+    $(".wardrobe").css({
+        "border": "5px solid #000000",
+        "height": maxHeight
     });
 }
